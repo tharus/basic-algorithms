62ff49957c085216a6341fb9dd0a4432930e6a39
Tharindu Dahanayake <tmsdahanayake@gmail.com>
2023-12-28 09:13:36 +0530

    Conventional Method

---
+++ php algorithms/dynamic programming/longest_increasing_subsequence.php
@@ -0,0 +1,30 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function lengthOfLIS($nums)
+    {
+        $n = count($nums);
+        if ($n === 0) {
+            return 0; // Handle empty array
+        }
+
+        $lenArr = array_fill(0, $n, 1); // Initialize with length 1 for all elements
+
+        for ($i = 1; $i < $n; $i++) {
+            for ($j = 0; $j < $i; $j++) {
+                if ($nums[$i] > $nums[$j]) {
+                    $lenArr[$i] = max($lenArr[$i], $lenArr[$j] + 1);
+                }
+            }
+        }
+
+        return max($lenArr);
+    }
+}
+
